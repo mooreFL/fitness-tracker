86d975d2571a1a8571b7e83dc512e0524b82ee89
mooreFL <moorepr09@gmail.com>
2021-05-13 16:29:51 -0400

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(require("./controllers"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
+
